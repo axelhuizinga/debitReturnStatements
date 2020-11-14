724a5d3c26f9a8e86f7fb1f0b398e79dee284427
axelhuizinga <axel@cunity.me>
2020-11-14 10:56:32 +0100

    first successfully tested version

---
+++ RLast.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * parse uploaded debit return statements 
+ */
+error_reporting(E_ERROR | E_PARSE | E_NOTICE);
+
+require_once('drs.php');
+
+?>
--- drs.php
+++ drs.php
@@ -2,6 +2,7 @@
 /**
  * parse uploaded debit return statements 
  */
+error_reporting(E_ERROR | E_PARSE | E_NOTICE);
 
 use Genkgo\Camt\Config;
 use Genkgo\Camt\Reader;
@@ -17,10 +18,11 @@ $loader->setNamespacePrefix('')
        ->register();
 
 #edump("FILES:".print_r($_FILES,1));
-edump("FILE:".implode('|',$_GET));
-edump("FILE:".implode('|',$argv));
+#edump("FILE:".implode('|',$_GET));
+#edump("FILE:".implode('|',$argv));
 if(count($_GET)==0 && $argc == 2)
 	$_GET['file'] = $argv[1];
+$appLog = dirname($argv[0]).'drs.log';
 #edump(strlen(file_get_contents($_GET['file'])));
 edump(print_r($_GET,1));
 if(file_exists($_GET['file']))
@@ -32,9 +34,7 @@ if(file_exists($_GET['file']))
 	$res = $z->open($_GET['file']);
 
 	if ($res === TRUE) {
-		/*while ($file = zip_read($z)) {
-			echo  zip_entry_name($file).PHP_EOL;
-		}*/
+		$drsGot = array();
 		$z->extractTo($tmp);
 		$od = opendir($tmp);
 		edump($z->count()."::".count(scandir($tmp)));
@@ -42,69 +42,100 @@ if(file_exists($_GET['file']))
 		{
 			edump($z->count()." Dateien entpackt");
 		}
+		$i=0;
+		while ($i<$z->count()) {
+			$drsGot = array_merge($drsGot, addDRS($z->getFromIndex($i++)));
+			#edump();
+		}
+
 	}
 	$z->close();
 	delTree($tmp);
+
 }
+#header('Content-Type: application/json');
+#echo json_encode(array('rlData'=>$rla),JSON_FORCE_OBJECT);
+echo count($drsGot)." Rücklastschriften extrahiert";
+#echo json_encode($drsGot);
+listAll($drsGot);
 exit('OK');
 
-$config = Config::getDefault();
-#$config->disableXsdValidation();
-##edump($config);
-$reader = new Reader($config);
-$dir = '/var/www/vhosts/pitverwaltung.de/files';
-#$reader = new Genkgo\Camt\Reader(Config::getDefault());
-#$message = $reader->readFile($dir.'/camt053.v2.minimal.xml');
-#$message = $reader->readFile($dir.'/camt/2014-01-03_C52_DE58740618130100033626_EUR_A00035.xml');
-#$message = $reader->readFile($dir.'/camt-1169231300-43060967-2020-09-23.xml');
-$message = $reader->readFile($_GET['file']);
-unlink($_GET['file']);
-#print_r($message);
-$s_i = 0;
-$r = 0;
-$t = 0;
-#edump(get_included_files());
-edump(strlen(print_r($message,1)));
-$statements = $message->getRecords();
-$count_statements = count($statements);
-#edump($statements);
-$rla = array();
-foreach ($statements as $statement) {		
-	$entries = $statement->getEntries();
-	$count_entries = count($entries);
-	edump($count_entries);
-	#edump($statement);
-	foreach($entries as $entry)
-	{
-		$money = $entry->getAmount();# Money object
-		#edump($entry);
-		### CHECK IF RECORD IS A RETURN TRANSACTION
-		if($rInfo = $entry->getTransactionDetail()->getReturnInformation()){
-			$traDet = $entry->getTransactionDetail();
-			$account = $traDet->getRelatedParties()[1]->getAccount();
-			#edump($traDet->getRelatedParties()[1]->getAccount()->getIdentification());
-			edump($rInfo->getCode().'::'.sprintf("%.2f", $money->getAmount()/100).'::'.$money->isNegative());			
-			edump('getReference:'.$traDet->getReference()->getMandateId().' getAccountServicerReference::'.$entry-> getAccountServicerReference().' getAccount::'.get_class($account).'<');
-			edump('getRemittanceInformation:'.$traDet->getRemittanceInformation()->getMessage().' getRelatedPartyAccount::'.$account->getIdentification().'  getEndToEndId::'.$traDet->getReference()->getEndToEndId());
-			$rlData = array(
-				'id'=>intval($traDet->getReference()->getMandateId()),
-				'baID'=>$traDet->getReference()->getEndToEndId(),
-				'iban'=>$account->getIdentification(),
-				'sepaCode'=>$rInfo->getCode(),
-				'dealId'=>$traDet->getReference()->getMandateId(),
-				'amount'=>sprintf("%.2f", $money->getAmount()/100)
-			);
-			array_push($rla,$rlData);
-		}
-		else{
-			### normal transaction						
-		}
+function listAll($list){
+	foreach($list as $l){
+		#echo(print_r($l, 1));
+		echo("id: $l[id] iban: $l[iban] Betrag: $l[amount]\r\n");
 	}
-	#print_r($entries);
 }
-	header('Content-Type: application/json');
-	#echo json_encode(array('rlData'=>$rla),JSON_FORCE_OBJECT);
-	echo json_encode(array('rlData'=>$rla));
+
+function addDRS($xml){
+
+	$config = Config::getDefault();
+	#$config->disableXsdValidation();
+	##edump($config);
+	$reader = new Reader($config);
+	$dir = '/var/www/vhosts/pitverwaltung.de/files';
+	#$reader = new Genkgo\Camt\Reader(Config::getDefault());
+	#$message = $reader->readFile($dir.'/camt053.v2.minimal.xml');
+	#$message = $reader->readFile($dir.'/camt/2014-01-03_C52_DE58740618130100033626_EUR_A00035.xml');
+	#$message = $reader->readFile($_GET['file']);
+	$message = $reader->readString($xml);
+	#$message = $reader->readFile($dir.'/camt-1169231300-43060967-2020-09-23.xml');
+	#print_r($message);
+	$s_i = 0;
+	$r = 0;
+	$t = 0;
+	#edump(get_included_files());
+	edump(strlen(print_r($message,1)));
+	$statements = $message->getRecords();
+	$count_statements = count($statements);
+	#edump($statements);
+	$rla = array();
+	foreach ($statements as $statement) {		
+		$entries = $statement->getEntries();
+		$count_entries = count($entries);
+		edump($count_entries);
+		#edump($statement);
+		foreach($entries as $entry)
+		{
+			$money = $entry->getAmount();# Money object
+			#edump($entry);
+			### CHECK IF RECORD IS A RETURN TRANSACTION
+			if($rInfo = $entry->getTransactionDetail()->getReturnInformation()){
+				#edump(print_r($entry->getTransactionDetails(),1));exit;
+				$traDet = $entry->getTransactionDetail();
+				$account = getAccount($traDet->getRelatedParties());#[1]->getAccount();
+				#edump($traDet->getRelatedParties()[1]->getAccount()->getIdentification());
+				edump($rInfo->getCode().'::'.sprintf("%.2f", $money->getAmount()/100).'::'.$money->isNegative());	
+				$iban = ($account==NULL?'':$account->getIdentification());
+				edump('getReference:'.$traDet->getReference()->getMandateId().' getAccountServicerReference::'.$entry-> getAccountServicerReference().' getAccount::'.get_class($account).'<');
+				edump('getRemittanceInformation:'.$traDet->getRemittanceInformation()->getMessage().' getRelatedPartyAccount::'.$iban.'  getEndToEndId::'.$traDet->getReference()->getEndToEndId());
+				$rlData = array(
+					'id'=>intval($traDet->getReference()->getMandateId()),
+					'baID'=>$traDet->getReference()->getEndToEndId(),
+					'iban'=>$iban,
+					'sepaCode'=>$rInfo->getCode(),
+					'dealId'=>$traDet->getReference()->getMandateId(),
+					'amount'=>sprintf("%.2f", $money->getAmount()/100)
+				);
+				array_push($rla,$rlData);
+			}
+			else{
+				### normal transaction						
+			}
+		}
+		#print_r($entries);
+	}
+	return $rla;
+}
+
+function getAccount($relParties)
+{
+	return array_pop($relParties)->getAccount();
+	/*foreach($relParties as $party){
+		if(strstr($party->relatedPartyDetails->name, 'SchutzengelWerk')==0)
+			continue
+	}*/
+}
 
 	function delTree($dir) {
 		$files = array_diff(scandir($dir), array('.','..'));
